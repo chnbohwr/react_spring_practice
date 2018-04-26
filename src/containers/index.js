a7a3f2309614bb2b5ec54de2aaa9e0725cfdfb6b
chnbohwr <chnbohwr@gmail.com>
2018-04-26 22:31:27 +0800

    add spring practice

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+import { Spring } from 'react-spring';
+
 export default class Main extends Component {
+  state = {
+    isShow: false,
+  }
+  toggleText = () => {
+    this.setState({ isShow: !this.state.isShow });
+  }
   render() {
+    const { isShow } = this.state;
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h2>Hello, I`m ReactMaker.</h2>
-        <p>To get started, edit containers/index.js</p>
+      <div>
+        <button onClick={this.toggleText}>click me to show text</button>
+        <Spring from={{ opacity: 0 }} to={{ opacity: isShow ? 1 : 0 }}>
+          {styles => <div style={styles}>i will fade in</div>}
+        </Spring>
       </div>
     );
   }
